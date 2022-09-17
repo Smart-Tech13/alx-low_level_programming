7b2bd39a4238612866533eb55df5ae21302bbf81
Smart-Tech13 <festusbaylin@gmail.com>
2022-09-16 17:43:26 -0700

    Write a function that prints an integer.

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,52 +1,28 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * print_number -  checks for checks for a digit (0 through 9).
- * @n: n -  Variable
- * Return: Always 0.
- */
+  * print_number - Prints an integer.
+  * @n: The integer to prints.
+  *
+  * Return: Nothing!
+  */
 void print_number(int n)
 {
-<<<<<<< HEAD
-	int i, d, p, f;
-	int j;
-	char num[1000];
-=======
-	unsigned int z;
-	int m, b;
->>>>>>> 02f4879befaee9c324521caa68f89e7cfec064bb
+	unsigned int k = n;
 
-	b = 10;
-
-	if (n < 10 && n >= 0)
+	if (n < 0)
 	{
-		_putchar (n + '0');
-	}
-	else if (n > -10 && n < 0)
-	{
-		n = n - 2 * n;
+		n *= -1;
+		k = n;
 		_putchar('-');
-		_putchar (n + '0');
 	}
 
-	else
-	{
-		if (n < 0)
-		{
-			n = n * -1;
-			_putchar ('-');
-		}
-		z = n;
-	while (z / b > 9)
-	{
-		b = b * 10;
-	}
-	while (b > 0)
-	{
-		m = z / b;
-		z = z % b;
-		_putchar (m + '0');
-		b = b / 10;
-	}
-	}
+	k /= 10;
+
+	if (k != 0)
+		print_number(k);
+
+	_putchar((unsigned int) n % 10 + '0');
+
 }
