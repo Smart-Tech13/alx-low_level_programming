80b211ef732410b3269ab3d3a39d073d2bc4d791
Smart-Tech13 <festusbaylin@gmail.com>
2022-10-10 20:53:39 -0700

    a function that frees dogs.
    Prototype: void free_dog(dog_t *d);

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+es (16 sloc)  215 Bytes
+
+#include "dog.h"
+#include <stdlib.h>
+/**
+* free_dog - a function that frees dogs
+* @d: pointer to struct
+* Return: nothing
+**/
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->owner);
+		free(d->name);
+		free(d);
+	}
+}
